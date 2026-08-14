2b1f5748897c585bb501cd92354fcfa0214f1ca4
oswaldsch <oswald.scheuren@gmx.de>
2026-08-14 23:13:10 +0200

    feat: wire backend into frontend

--- static/main.js
+++ static/main.js
@@ -1,8 +1,11 @@
-async function newNote() {
+async function saveNote() {
+  const noteTitleInput = document.getElementById("noteTitleInput")
+  const noteContentInput = document.getElementById("noteContentInput")
+  const dialog = document.getElementById("newNoteModal")
   const payload = {
-    "title": "Testing",
-    "content": "# This is a heading\nThis is a text"
-  };
+    title: noteTitleInput.value,
+    content: noteContentInput.value
+  }
 
   try {
     const response = await fetch("/api/new_note", {
@@ -15,7 +18,27 @@ async function newNote() {
 
     const result = await response.json();
     console.log("Created new note:", result);
+    dialog.close()
   } catch (error) {
     console.error("Error creating new note:", error);
   }
 }
+
+async function getNotes() {
+  try {
+    const response = await fetch("/api/notes")
+    const { notes } = await response.json()
+    const noteArea = document.getElementById("noteArea")
+    notes.forEach((note, index) => {
+      const noteCard = document.createElement("div")
+      noteCard.classList.add("note-card")
+      noteCard.innerHTML = `<h1 class="note-title">${note.title}</h1><div class="note-desc">${marked.parse(note.content)}</div>`;
+      noteArea.appendChild(noteCard);
+    });
+  }
+  catch (error) {
+    console.error("Error creating new note:", error);
+  }
+}
+
+getNotes()
